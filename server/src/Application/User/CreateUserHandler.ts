4f5fc610b101a25b1ab4c732eac9cb3e77747afb
laubernal <lbernalrodrguez@gmail.com>
2024-11-01 11:58:04 +0100

    feat: implement create user handler

---
+++ server/src/Application/User/CreateUserHandler.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { USERS_REPOSITORY } from 'src/Constants';
+import { User } from 'src/Domain/Entities/User';
+import { UserAlreadyExistsError } from 'src/Domain/Errors/UserAlreadyExists';
+import { IUserRepository } from 'src/Domain/Repositories/IUserRepository';
+
+@Injectable()
+export class CreateUserHandler {
+  constructor(
+    @Inject(USERS_REPOSITORY) private readonly repository: IUserRepository,
+  ) {}
+
+  public async execute(body: any): Promise<void> {
+    await this.ensureEmailIsNotUsed(body.email);
+
+    const newUser = User.build(
+      body.id,
+      body.name,
+      body.email,
+      body.password,
+      body.isActive,
+      new Date(),
+      new Date(),
+    );
+
+    await this.repository.save(newUser);
+  }
+
+  private async ensureEmailIsNotUsed(email: string): Promise<void> {
+    const user = await this.repository.findByEmail(email);
+
+    if (user) {
+      throw new UserAlreadyExistsError();
+    }
+  }
+}
