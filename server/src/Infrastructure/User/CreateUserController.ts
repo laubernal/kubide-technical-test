d4e2486ed7e4439f9ad630bec1078beb8e8a1b7b
laubernal <lbernalrodrguez@gmail.com>
2024-11-01 12:28:53 +0100

    feat: implement create user controller

---
+++ server/src/Infrastructure/User/CreateUserController.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AppResponse } from 'src/AppResponse';
+import { CreateUserDto } from './CreateUserDto';
+import { CreateUserHandler } from 'src/Application/User/CreateUserHandler';
+
+@Controller()
+export class CreateUserController {
+  constructor(private readonly createUserHandler: CreateUserHandler) {}
+
+  @Post('/api/auth/register')
+  public async post(
+    @Body() body: CreateUserDto,
+    @Res() res: Response,
+  ): Promise<Response<any, Record<string, any>>> {
+    try {
+      await this.createUserHandler.execute(body);
+
+      const response = new AppResponse(null, {
+        success: true,
+        error: null,
+      });
+
+      return res.status(200).json(response);
+    } catch (error: any) {
+      const errorResponse = new AppResponse(null, {
+        success: false,
+        error: error.message,
+      });
+
+      return res.status(400).json(errorResponse);
+    }
+  }
+}
