2edde7d0612227090d2bd24c9e9c37044a7c4c41
laubernal <lbernalrodrguez@gmail.com>
2024-11-01 15:20:08 +0100

    feat: add postgres and typeorm connectio to module definition

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CreateUserController } from './Infrastructure/Controllers/User/CreateUserController';
+import { CreateUserHandler } from './Application/User/CreateUserHandler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const controllers = [CreateUserController];
+
+const handlers = [CreateUserHandler];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [...controllers],
+  providers: [...handlers],
 })
 export class AppModule {}
