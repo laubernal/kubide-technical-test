c7735f1a6c02bf7c86f0f6ea00b79be3916f5e24
laubernal <lbernalrodrguez@gmail.com>
2024-11-02 15:36:04 +0100

    fix: remove commented code

--- server/src/Infrastructure/Controllers/UpdateUser/UpdateUserRequest.ts
+++ server/src/Infrastructure/Controllers/UpdateUser/UpdateUserRequest.ts
@@ -1,15 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserRequest {
-//   @IsString()
-//   @IsNotEmpty()
-//   @ApiProperty({
-//     example: '786c3648-fec0-419f-8131-157d02d58d38',
-//     description: 'The id of the user',
-//   })
-//   id: string;
-
   @IsString()
   @IsOptional()
   @ApiProperty({
