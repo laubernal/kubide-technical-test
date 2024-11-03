a9ea1d2be31ab3dcfd294f226bbd220ad76b12a2
laubernal <lbernalrodrguez@gmail.com>
2024-11-03 12:00:03 +0100

    feat: implement migration to create notifications table

---
+++ server/typeorm/migrations/1730631454899-CreateNotificationsTable.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateNotificationsTable1730631454899
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "notifications" (
+                no_id VARCHAR PRIMARY KEY,
+                no_messages_id VARCHAR REFERENCES messages(me_id),
+                no_receiver_id VARCHAR REFERENCES users(us_id),
+                no_created_at TIMESTAMP NOT NULL DEFAULT now()
+            );`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "notifications"`);
+  }
+}
